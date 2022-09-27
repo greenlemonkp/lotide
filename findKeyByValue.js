48a8917cd72bb7e631b71cd63d9cdfe4dbbd40c1
greenlemonkp <greenlemonkp@gmail.com>
2022-09-27 12:14:00 -0400

    findkeybyvalue

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🤢🤢🤢 Assertion failed ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  const keys = Object.keys(object);
+  let result
+  for (let k of keys) {
+     
+    if(object[k] === value) {
+      result = k
+    }
+ } 
+ return result;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
