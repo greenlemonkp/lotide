a764256324278f0fd4f8beb9323c5974bf49318f
greenlemonkp <greenlemonkp@gmail.com>
2022-10-12 11:39:52 -0400

    refactorModule

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,10 @@
-//function implementation
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("👍👍👍Assertion passed: " + actual + "===" + expected);
+    console.log(`👍👍👍 Assertion passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("🤢🤢🤢Assertion failed " + actual + "!==" + expected);
+    console.log(`🤢🤢🤢 Assertion failed ${actual} !== ${expected}`);
   }
 };
 
-//TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual(1, 91);
-assertEqual('Sushi', 'Sushi');
+
+module.exports = assertEqual;
