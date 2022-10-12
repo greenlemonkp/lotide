2776716b9fda2f78f299254f371a8be68a7544b2
greenlemonkp <greenlemonkp@gmail.com>
2022-10-12 11:40:21 -0400

    refactorModule

--- head.js
+++ head.js
@@ -1,21 +1,8 @@
-//function implementation
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🤢🤢🤢 Assertion failed ${actual} !== ${expected}`);
-  }
-};
-
 // eslint-disable-next-line func-style
-function head (arr) {
+ const head = function (arr) {
   if (arr.length === 0) {
     return undefined;
   }
   return arr[0];
-}
-
-
-//TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+};
+module.exports = head;
