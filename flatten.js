a74f81dc4b23263f57797b259703130513c6f7cd
greenlemonkp <greenlemonkp@gmail.com>
2022-10-14 12:23:51 -0400

    Ready to publish npm package

--- flatten.js
+++ flatten.js
@@ -1,8 +1,3 @@
-const assertArraysEqual = require('./assertArraysEqual');
-
-
-const eqArrays = require('./eqArrays');
-
 // eslint-disable-next-line func-style
 function flatten(arr) {
   let result = [];
@@ -16,7 +11,5 @@ function flatten(arr) {
   }
   return result;
 }
-
-// console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
 
 module.exports = flatten;
