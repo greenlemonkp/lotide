ace844c0d1a818f99d3ddd195c6d828b588c7273
greenlemonkp <greenlemonkp@gmail.com>
2022-10-14 10:23:16 -0400

    refactor

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,6 @@
 
 // eslint-disable-next-line func-style
-function eqArrays (firstArray, secondArray) {
+let eqArrays = function(firstArray, secondArray) {
   
   let isEqual = firstArray.length === secondArray.length&&
   firstArray.every((value, index) => value === secondArray[index]);
