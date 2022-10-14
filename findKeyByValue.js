643fa3197dd9099740a32584d9e526690a69118a
greenlemonkp <greenlemonkp@gmail.com>
2022-10-14 10:24:51 -0400

    refactor

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,28 +1,24 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🤢🤢🤢 Assertion failed ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-const findKeyByValue = function(object, value) {
+const findKeyByValue = function (object, value) {
   const keys = Object.keys(object);
-  let result
+  let result;
   for (let k of keys) {
-     
-    if(object[k] === value) {
-      result = k
-    }
- } 
- return result;
-}
 
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
+    if (object[k] === value) {
+      result = k;
+    }
+  }
+  return result;
 };
 
-console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);//
+// const bestTVShowsByGenre = { 
+//   sci_fi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama:  "The Wire"
+// };
+
+// console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);//
+
+module.exports = findKeyByValue;
