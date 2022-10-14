f02eff334b9d515a42f110f1914ad0488864d563
greenlemonkp <greenlemonkp@gmail.com>
2022-10-14 10:24:22 -0400

    refactor

--- findKey.js
+++ findKey.js
@@ -1,10 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🤢🤢🤢 Assertion failed ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 
 const findKey = function(names, callback) {
   const keys = Object.keys(names);
@@ -21,12 +16,14 @@ const findKey = function(names, callback) {
 
 
   
-//test code
-console.log(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2)) // => "noma"
+// //test code
+// console.log(findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2)) // => "noma"
+
+module.exports = findKey;
