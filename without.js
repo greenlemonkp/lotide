fcc7b2e4f942512d42eace18d3b1aac9d9395a8a
greenlemonkp <greenlemonkp@gmail.com>
2022-10-14 12:26:10 -0400

    Ready to publish npm package

--- without.js
+++ without.js
@@ -1,21 +1,3 @@
-const assertArraysEqual = function(firstArray, secondArray) {
-  if (firstArray.length === secondArray.length &&
-    firstArray.every((value, index) => value === secondArray[index])) {
-    console.log(` Assertion passed: ${firstArray} === ${secondArray}`);
-  } else {
-    console.log(` Assertion failed ${firstArray} !== ${secondArray}`);
-  }
-};
-
-// eslint-disable-next-line func-style
-function eqArrays(firstArray, secondArray) {
-  
-  let isEqual = firstArray.length === secondArray.length &&
-  firstArray.every((value, index) => value === secondArray[index]);
-  console.log(isEqual);
-}
-
-// eslint-disable-next-line func-style
 function without(source, itemsToRemove) {
   let result = [];
 
@@ -33,12 +15,5 @@ function without(source, itemsToRemove) {
   }
   return result;
 }
-// console.log(without([1, 2, 3], [1])) // => [2, 3]
-// console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"])
-
-// const words = ["hello", "world", "lighthouse"];
-// without(words, ["lighthouse"]); // no need to capture return value for this test case
-// // Make sure the original array was not altered by the without function
-// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
 module.exports = without;
