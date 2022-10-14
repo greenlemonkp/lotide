33f5f9b3364eff497376f7bd8cb2f77b8d5e4184
greenlemonkp <greenlemonkp@gmail.com>
2022-10-14 12:27:03 -0400

    Ready to publish npm package

---
+++ takeUntil.js
@@ -0,0 +1,13 @@
+const takeUntil = function(array, callback) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      break;
+    }
+    result.push(array[i]);
+  }
+  return result;
+};
+
+
+module.exports = takeUntil;
