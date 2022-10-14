4139b95a7ee5ec25960cdbfb2f29fa01d016de99
greenlemonkp <greenlemonkp@gmail.com>
2022-10-14 12:22:36 -0400

    Ready to publish npm package

--- eqObjects.js
+++ eqObjects.js
@@ -30,11 +30,4 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-// const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
-// assertEqual(eqObjects(cd, dc), true); // => true
-
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-// assertEqual(eqObjects(cd, cd2), false); // => false
-
 module.exports = eqObjects;
