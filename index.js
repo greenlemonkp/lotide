e3c3e2b7d1bb66d15dd087d219cdf13fcc28d3c2
greenlemonkp <greenlemonkp@gmail.com>
2022-10-14 10:25:37 -0400

    update

--- index.js
+++ index.js
@@ -2,9 +2,38 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const assertArraysEqual =  require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+// const map = require('./map');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
 
 module.exports = {
   head:   head,
   tail:   tail,
-  middle: middle
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  eqArrays: eqArrays,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  // map: map,
+  without: without,
+  takeUntil: takeUntil
+
 };
