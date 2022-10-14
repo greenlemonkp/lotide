b186660ee4e03aa8623daff32430449137563673
greenlemonkp <greenlemonkp@gmail.com>
2022-10-14 10:26:28 -0400

    refactor

--- without.js
+++ without.js
@@ -33,10 +33,12 @@ function without(source, itemsToRemove) {
   }
   return result;
 }
-console.log(without([1, 2, 3], [1])) // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"])
+// console.log(without([1, 2, 3], [1])) // => [2, 3]
+// console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"])
 
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+// const words = ["hello", "world", "lighthouse"];
+// without(words, ["lighthouse"]); // no need to capture return value for this test case
+// // Make sure the original array was not altered by the without function
+// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+module.exports = without;
