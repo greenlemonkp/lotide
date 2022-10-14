98945843739ed02bc9e5d9ba53919fe8839ebffa
greenlemonkp <greenlemonkp@gmail.com>
2022-10-14 12:25:11 -0400

    Ready to publish npm package

--- map.js
+++ map.js
@@ -1,34 +1,11 @@
-const assertArraysEqual = function(firstArray, secondArray) {
-  if (firstArray.length === secondArray.length &&
-    firstArray.every((value, index) => value === secondArray[index])) {
-    console.log(`👍👍👍 Assertion passed: ${firstArray} === ${secondArray}`);
-  } else {
-    console.log(`🤢🤢🤢 Assertion failed ${firstArray} !== ${secondArray}`);
-  }
-};
+const map = function (array, callback) {
 
-function eqArrays (firstArray, secondArray) {
-  
-  let isEqual = firstArray.length === secondArray.length&&
-  firstArray.every((value, index) => value === secondArray[index]);
-  console.log(isEqual);
-}
-
-
-
-const words = ["ground", "control", "to", "major", "tom"];
-
-const map = function(array, callback) {
- 
   const results = [];
   for (let item of array) {
     results.push(callback(item))
-      }
+  }
   return results;
-  
-}
-const map1 = map(words, word => word[0]);
 
+};
 
-
-console.log(assertArraysEqual(map1));
+module.exports = map;
