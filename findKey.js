115e163900bbbf8ae1d5040a88e6efbcc18319b4
greenlemonkp <greenlemonkp@gmail.com>
2022-10-21 12:17:01 -0400

    added comments to show outcome of the function and ready for publish ver.1.0.2

--- findKey.js
+++ findKey.js
@@ -1,6 +1,6 @@
-const findKey = function(names, callback) {
+const findKey = function(names, callback) { //scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined
   const keys = Object.keys(names);
-  //const keys2 = Object.values(names)
+  
 
   for (let j of keys) {
     if (callback(names[j])) {
