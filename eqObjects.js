8cdf2ead935d8d6ed3bb74a0db61b0fd82e07bf8
greenlemonkp <greenlemonkp@gmail.com>
2022-10-21 12:16:50 -0400

    added comments to show outcome of the function and ready for publish ver.1.0.2

--- eqObjects.js
+++ eqObjects.js
@@ -5,7 +5,7 @@ const eqArrays = require('./eqArrays');
 
 
 // Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
+// Otherwise you get back a big fat false!!!
 const eqObjects = function(object1, object2) {
   let key1 = Object.keys(object1);
   let key2 = Object.keys(object2);
