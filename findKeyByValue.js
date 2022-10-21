c05b91237db24f3314cefabe487d7f76f254299e
greenlemonkp <greenlemonkp@gmail.com>
2022-10-21 12:35:50 -0400

    added comments to show outcome of the function and ready for publish ver.1.0.2

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,10 @@
 const findKeyByValue = function (object, value) {
   const keys = Object.keys(object);
-  let result;
   for (let k of keys) {
 
     if (object[k] === value) {
-      result = k;
-      return result;
+      
+      return k;
     }
   }
   
