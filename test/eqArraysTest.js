e39b55af791c3a58d07b6a4960b6bab972a27be2
greenlemonkp <greenlemonkp@gmail.com>
2022-10-21 11:42:14 -0400

    added more example testing code

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,8 +1,8 @@
 const assertEqual = require('../assertEqual');
-const eqArrays = require('../eqArrays')
+const eqArrays = require('../eqArrays');
 
 
-eqArrays([1,2,3], [1,2,3]);
-eqArrays(["1", "2", "3"], ["1", "2", "3"])
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3])); // => should PASS
+console.log(eqArrays([1,2,3], [1,2,3]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]))
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3])); // => should PASS
