61126732e4ca00caac1f330db7d6a5ffcf5b281d
greenlemonkp <greenlemonkp@gmail.com>
2022-10-21 12:18:11 -0400

    added comments to show outcome of the function and ready for publish ver.1.0.2

--- letterPositions.js
+++ letterPositions.js
@@ -1,13 +1,12 @@
 const letterPositions = function (sentence) {
   const results = {};
-  // logic to update results here
   for (let i = 0; i < sentence.length; i++) {
     if (sentence[i] !== ' ') {
       let newKey = sentence[i]; //iterate each element
       if (results[newKey]) { //if newKey is already present, push position into it
         results[newKey].push(i);
       } else {
-        results[newKey] = [i]; //newKey is element in the string 
+        results[newKey] = [i]; //newKey is element in the string
       }
     }
   }
