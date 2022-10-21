041d6f0b5ac453f15bb8ee2a665a3652949a980d
greenlemonkp <greenlemonkp@gmail.com>
2022-10-21 12:18:38 -0400

    added comments to show outcome of the function and ready for publish ver.1.0.2

--- map.js
+++ map.js
@@ -1,8 +1,8 @@
-const map = function (array, callback) {
+const map = function (array, callback) { //return a new array based on the results of the callback function
 
   const results = [];
   for (let item of array) {
-    results.push(callback(item))
+    results.push(callback(item));
   }
   return results;
 
