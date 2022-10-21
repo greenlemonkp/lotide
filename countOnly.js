94091ea45a2905b6232e5b9a033cab4343033c45
greenlemonkp <greenlemonkp@gmail.com>
2022-10-21 12:16:15 -0400

    added comments to show outcome of the function and ready for publish ver.1.0.2

--- countOnly.js
+++ countOnly.js
@@ -1,7 +1,7 @@
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 
-const countOnly = function(allItems, itemsToCount) {
+const countOnly = function(allItems, itemsToCount) { //returns count(number) of itemsToCount in allItems
   let result = {};
   for (let items in itemsToCount) {
     // eslint-disable-next-line no-prototype-builtins, no-undef
