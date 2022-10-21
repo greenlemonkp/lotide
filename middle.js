4e6cfc2e3917b9b333bdee689a2be0516e54de65
greenlemonkp <greenlemonkp@gmail.com>
2022-10-21 12:18:56 -0400

    added comments to show outcome of the function and ready for publish ver.1.0.2

--- middle.js
+++ middle.js
@@ -1,5 +1,5 @@
 const middle = function(array) {
-
+//will take in an array and return the middle-most element(s) of the given array.
   const length = array.length;
   const middleIndex = Math.floor(length / 2);
  
@@ -9,7 +9,7 @@ const middle = function(array) {
 
   if (length % 2 === 0) {
     const left = array[middleIndex] - 1;
-    console.log(left)
+    console.log(left);
     const right = array[middleIndex];
     return [left, right];
   } else {
