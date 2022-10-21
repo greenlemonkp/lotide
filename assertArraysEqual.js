ffce173fcd315f3bd21845769805d10db70f2da8
greenlemonkp <greenlemonkp@gmail.com>
2022-10-21 12:14:50 -0400

    added comments to show outcome of the function and ready for publish ver.1.0.2

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,5 @@
-const assertArraysEqual = function(firstArray, secondArray) {
+const assertArraysEqual = function(firstArray, secondArray) { // returns PASS or FAIL according to first and second arrys
+
   if (firstArray.length === secondArray.length &&
     firstArray.every((value, index) => value === secondArray[index])) {
     console.log(`👍👍👍 Assertion passed: ${firstArray} === ${secondArray}`);
