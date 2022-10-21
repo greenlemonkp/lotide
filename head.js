5c8e990db253e5c66007ebdf47d0edf8566b7394
greenlemonkp <greenlemonkp@gmail.com>
2022-10-21 12:17:25 -0400

    added comments to show outcome of the function and ready for publish ver.1.0.2

--- head.js
+++ head.js
@@ -1,8 +1,12 @@
 // eslint-disable-next-line func-style
- const head = function (arr) {
-  if (arr.length === 0) {
+const head = function (arr) { //returns first item in the array or returns undefined if not array or undefined
+  if (arr.length === 0 || arr === undefined) {
     return undefined;
   }
   return arr[0];
 };
+
+console.log((head([5,6,7]), 5));
+console.log((head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+console.log(undefined);
 module.exports = head;
