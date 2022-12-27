9084b90ed246b721a858d80cd1244f138c2e3cc5
JOON LEE <110060709+greenlemonkp@users.noreply.github.com>
2022-12-27 09:41:19 -0500

    Test to see if cloned successful

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,14 @@
-const assertArraysEqual = function(firstArray, secondArray) { // returns PASS or FAIL according to first and second arrys
+const assertArraysEqual = function (firstArray, secondArray) {
+  // returns PASS or FAIL according to first and second arrys
 
-  if (firstArray.length === secondArray.length &&
-    firstArray.every((value, index) => value === secondArray[index])) {
+  if (
+    firstArray.length === secondArray.length &&
+    firstArray.every((value, index) => value === secondArray[index])
+  ) {
     console.log(`👍👍👍 Assertion passed: ${firstArray} === ${secondArray}`);
   } else {
-    console.log(`🤢🤢🤢 Assertion failed ${firstArray} !== ${secondArray}`);
+    console.log(`🤢🤬🤢🤬🤢 Assertion failed ${firstArray} !== ${secondArray}`);
   }
 };
-
 
 module.exports = assertArraysEqual;
